1bba589da1e7316813bdd4bd287c6346169c75ce
morenoalexander <am@alexandermoreno.com>
2022-05-05 08:28:46 -0500

    updated LandingPage

--- pages/LandingPage.tsx
+++ pages/LandingPage.tsx
@@ -1,26 +1,32 @@
-import React from "react"
-import { StyleSheet, View, Text, Button } from "react-native"
-import { StatusBar } from "expo-status-bar"
-import tailwind from "tailwind-rn"
-import {createStackNavigator, StackNavigationProp} from "@react-navigation/stack";
+import React from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import tailwind from "tailwind-rn";
+import {
+  createStackNavigator,
+  StackNavigationProp,
+} from "@react-navigation/stack";
+import GLButton from "../components/common/GLButton";
 
-
-
-export default function LandingPage({navigation} : {
-    navigation: StackNavigationProp<any>
-} ) {
+export default function LandingPage({
+  navigation,
+}: {
+  navigation: StackNavigationProp<any>;
+}) {
   return (
     <View style={styles.container}>
       <Text style={styles.Text}>Welcome to the Grocery App</Text>
       <Button title="Log in" onPress={() => console.log("Log in!")} />
-      <Button title="Sign Up" onPress={() => navigation.push("SignUp")} />
+      <GLButton onPress={() => navigation.push("SignUp")}>
+        <Text>Register</Text>
+      </GLButton>
       <Button
         title="Continue without registration!"
         onPress={() => console.log("Continue without registration!")}
       />
       <StatusBar style="auto" />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -31,4 +37,4 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   Text: tailwind("text-green-600"),
-})
+});
